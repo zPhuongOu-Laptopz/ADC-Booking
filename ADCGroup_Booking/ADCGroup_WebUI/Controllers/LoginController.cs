876f41e49584db74045bacdcc4e12709af99ec37
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2017-10-21 18:25:55 +0700

    InitialCreate Version Alpha v1.0
    - Create Project.
    - Design Login Web UI.
    - Update Validation for Login.
    - Use Asp.net MVC.

---
+++ ADCGroup_Booking/ADCGroup_WebUI/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using ADCGroup_WebUI.Models.LoginModels;
+using System.Web.Mvc;
+
+namespace ADCGroup_WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Home(Accounts account)
+        {
+            if (account.Username == null)
+            {
+                ModelState.AddModelError("Notification", "Bạn chưa nhập Username");
+            }
+            else if (account.Password == null)
+            {
+                ModelState.AddModelError("Notification", "Bạn chưa nhập Password");
+            }
+            else
+            {
+                ModelState.AddModelError("Notification", "Đăng nhập thành công");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ ADCGroup_Booking/ADCGroup_WebUI/Models/LoginModels/Accounts.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADCGroup_WebUI.Models.LoginModels
+{
+    public class Accounts
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
