f4a5e1ece09ffcaa50992e6d569ec6307c978cd1
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2017-10-27 08:52:32 +0700

    Update Version Alpha v1.0.5

--- ADCGroup_Booking/ADCGroup_WebUI/Controllers/HomeController.cs
+++ ADCGroup_Booking/ADCGroup_WebUI/Controllers/HomeController.cs
@@ -15,5 +15,17 @@ namespace ADCGroup_WebUI.Controllers
             ViewBag.Name = username;
             return View();
         }
+
+        [HttpGet]
+        public ActionResult MeetingRoom()
+        {
+            return View();
+        }
+
+        public string OpenModelPopup()
+        {
+            //can send some data also.
+            return "<h1>This is Modal Popup Window</h1>";
+        }
     }
 }
